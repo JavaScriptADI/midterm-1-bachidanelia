dbb78856292950c27579aa04e84662af77758034
Vaja <104520070+bachidanelia@users.noreply.github.com>
2024-11-29 13:46:23 +0400

    Add files via upload

---
+++ src/task57.js
@@ -0,0 +1,8 @@
+//57.Write a JavaScript program to create one string of specified copies (positive numbers) of a given string.
+function makaCopies(a, b) {
+    for (let i = 0; i < b; i++) {
+        console.log(a);
+    };
+};
+
+makaCopies("makarena", 4); // makarena makarena makarena makarena
